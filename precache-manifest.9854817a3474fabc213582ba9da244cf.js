e45f257716e466dc80d58e364c307e9f71b8a89c
Andreas_Just <andreas.wq@gmail.com>
2019-11-14 20:50:58 +0200

    Updates

---
+++ precache-manifest.9854817a3474fabc213582ba9da244cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4be88badf5021c16ace91020cc8e6735",
+    "url": "/react-redux-blank/index.html"
+  },
+  {
+    "revision": "f2a3387f0247ea2f1c2f",
+    "url": "/react-redux-blank/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ecba07a746541ffa55d2",
+    "url": "/react-redux-blank/static/js/2.e6347c8e.chunk.js"
+  },
+  {
+    "revision": "f2a3387f0247ea2f1c2f",
+    "url": "/react-redux-blank/static/js/main.7fafbbdb.chunk.js"
+  },
+  {
+    "revision": "cf59613e2ecb1cf88e45",
+    "url": "/react-redux-blank/static/js/runtime-main.4aff9625.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-redux-blank/static/media/logo.25bf045c.svg"
+  }
+]);
